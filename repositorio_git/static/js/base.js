668006694c4325a690978223894aad520d8ad927
Bernardo Moraes <147925451+bernardo-tardin@users.noreply.github.com>
2025-10-21 17:21:56 +0100

    Delete repositorio_git directory

--- repositorio_git/static/js/base.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("toggle-btn").addEventListener("click", function () {
-      const sidebar = document.getElementById("sidebar");
-      sidebar.classList.toggle("collapsed");
-
-      const title = document.getElementById("sidebar-title");
-      title.style.display = sidebar.classList.contains("collapsed") ? "none" : "block";
-  });
--- repositorio_git/ward_data_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class WardDataAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ward_data_app'
-    # Updated verbose name for the admin panel
-    verbose_name = 'Ward Data App'
